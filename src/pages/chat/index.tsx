ebf6a42a84b225f6289a248454c89d5653212dc3
Joonatan Korpela <joonatan.korpela@aalto.fi>
2023-11-10 22:19:34 +0200

    Add chat

--- src/pages/chat/index.tsx
+++ src/pages/chat/index.tsx
@@ -1,7 +1,47 @@
+import { useState } from 'react';
+
+interface Message {
+  byUser: boolean;
+  text: string;
+}
+
 function Chat() {
+  const [value, setValue] = useState('');
+  const [messages, setMessages] = useState<Message[]>([]);
+  const [answerLoading, setAnswerLoading] = useState(false);
+
+  const sendMessage = () => {
+    if (answerLoading || !value) return;
+    setMessages((prev) => [...prev, { byUser: true, text: value }]);
+    setValue('');
+    setAnswerLoading(true);
+    setTimeout(() => {
+      setMessages((prev) => [...prev, { byUser: false, text: 'Reply' }]);
+      setAnswerLoading(false);
+    }, 1500);
+  };
+
   return (
-    <div>
-      <h1>Chat</h1>
+    <div className="flex flex-col items-center justify-end w-full h-[calc(100vh-140px)] overflow-y-scroll">
+      <div className="w-full max-w-[800px] py-6">
+        {messages.map((message, i) => (
+          <div className={`chat ${message.byUser ? 'chat-end' : 'chat-start'}`}>
+            <div className="chat-bubble">{message.text}</div>
+          </div>
+        ))}
+        {answerLoading && <span className="loading loading-dots loading-lg mx-6 absolute -translate-y-8" />}
+      </div>
+      <textarea
+        className="textarea textarea-bordered w-full h-[80px] max-w-[800px] flex-shrink-0 resize-none"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        onKeyDown={(e) => {
+          if (e.key === 'Enter' && !e.shiftKey) {
+            e.preventDefault();
+            sendMessage();
+          }
+        }}
+      />
     </div>
   );
 }
