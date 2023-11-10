e3318be112f32a921463af326f40a2a41860e2b1
Simon Nars <simon.nars@gmail.com>
2023-11-11 00:09:10 +0200

    fix

--- src/pages/input/index.tsx
+++ src/pages/input/index.tsx
@@ -1,10 +1,21 @@
 import { useUserDocument } from '@/lib/store';
+import { Timestamp, arrayUnion, doc, updateDoc } from 'firebase/firestore';
+import { httpsCallable } from 'firebase/functions';
+import { StringFormat } from 'firebase/storage';
 import { useEffect, useState } from 'react';
+import { useFirestore, useFunctions, useUser } from 'reactfire';
 
 function Input() {
   const { data, status, set } = useUserDocument('SpgDdCaYeUSU6Nt9MzlW');
   const [value, setValue] = useState('');
   const [painLevel, setPainLevel] = useState(1);
+
+  const [response, setResponse] = useState('');
+
+  const OpenAIAssistantPainInputFunction = httpsCallable(useFunctions(), 'OpenAIAssistantPainInput');
+
+  const user = useUser();
+  const db = useFirestore();
 
   useEffect(() => {
     status === 'success' && setValue(data?.test);
@@ -15,8 +26,32 @@ function Input() {
     setPainLevel(parseInt(newValue.target.value));
   };
 
+  const onSubmit = async () => {
+    console.log('running');
+    const res = await OpenAIAssistantPainInputFunction({
+      description: value,
+    });
+    // @ts-ignore
+    const jsonContent = res.data[0].text.value.match(/```json\n([\s\S]*?)\n```/)[1];
+    const parsedObject = JSON.parse(jsonContent);
+
+    const docRef = doc(db, 'users', user.data!.uid);
+    await updateDoc(docRef, {
+      logs: arrayUnion({
+        timestampCreated: Timestamp.now(),
+        activity: parsedObject.activity_of_the_user,
+        bodyPart: parsedObject.body_part,
+        trigger: parsedObject.trigger,
+        vitalInfo: parsedObject.vital_information,
+      }),
+    });
+
+    console.log(parsedObject);
+    setResponse(res.data as any);
+  };
+
   return (
-    <div className="flex flex-col items-center">
+    <div className="flex flex-col items-center pb-32">
       <header>
         <h1 style={{ fontSize: '40px', fontWeight: 'bold', marginBottom: '20px' }}>
           How have you been feeling recently?
@@ -28,40 +63,59 @@ function Input() {
         </p>
       </header>
 
-      <div style={{ width: '68%' }}>
-        <input
-          type="range"
-          min={1}
-          max="5"
-          value={painLevel}
-          onChange={updateSliderValue}
-          className="range range-error"
-          step="1"
-        />
-        <div className="w-full flex justify-between text-s px-2">
-          {[1, 2, 3, 4, 5].map((label) => (
-            <span key={label}>{label}</span>
-          ))}
+      <form
+        action=""
+        className="w-[1000px]"
+        onSubmit={(e) => {
+          e.preventDefault();
+          onSubmit();
+        }}
+      >
+        <div style={{ width: '68%' }}>
+          <input
+            type="range"
+            min={1}
+            max="5"
+            value={painLevel}
+            onChange={updateSliderValue}
+            className="range range-error"
+            step="1"
+          />
+          <div className="w-full flex justify-between text-s px-2">
+            {[1, 2, 3, 4, 5].map((label) => (
+              <span key={label}>{label}</span>
+            ))}
+          </div>
         </div>
-      </div>
 
-      {loading ? (
-        <span className="loading loading-spinner loading-lg" />
-      ) : (
-        <textarea
-          className="textarea textarea-bordered w-full max-w-[900px] h-[200px] my-10"
-          placeholder="Test"
-          value={value}
-          onChange={(e) => setValue(e.target.value)}
-          onKeyDown={(e) => {
-            if (e.key === 'Enter') {
-              e.preventDefault();
-              set({ test: value, painLevel });
-            }
-          }}
-        />
-      )}
-      {JSON.stringify(data)}
+        {loading ? (
+          <span className="loading loading-spinner loading-lg" />
+        ) : (
+          <textarea
+            className="textarea textarea-bordered w-full max-w-[900px] h-[200px] my-10"
+            placeholder="Test"
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+            onKeyDown={(e) => {
+              // if (e.key === 'Enter') {
+              //   e.preventDefault();
+              //   set({ test: value, painLevel });
+              // }
+            }}
+          />
+        )}
+        <button className="py-2 px-6 bg-blue-200 rounded-md font-bold" type="submit">
+          Submit
+        </button>
+        <div>
+          {/* {JSON.stringify(value)}
+          {JSON.stringify(data)} */}
+          <div>
+            Response:
+            {JSON.stringify(response)}
+          </div>
+        </div>
+      </form>
     </div>
   );
 }
