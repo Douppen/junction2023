bf35f97e6796d0bdc400ea543e61ff6565d30d0b
danielmichaeli <daniel.michaeli@aalto.fi>
2023-11-10 20:46:23 +0200

    newest index page

--- src/pages/input/index.tsx
+++ src/pages/input/index.tsx
@@ -1,7 +1,35 @@
+import { useUserDocument } from '@/lib/store';
+import { useEffect, useState } from 'react';
+
 function Input() {
+  const { data, status, set } = useUserDocument('SpgDdCaYeUSU6Nt9MzlW');
+  const [value, setValue] = useState('');
+
+  useEffect(() => {
+    status === 'success' && setValue(data?.test);
+  }, [data, status]);
+
+  const loading = status === 'loading';
+
   return (
-    <div>
-      <h1>Input</h1>
+    <div className="flex flex-col items-center">
+      {loading ? (
+        <span className="loading loading-spinner loading-lg" />
+      ) : (
+        <textarea
+          className="textarea textarea-bordered w-full max-w-[500px] h-[200px] my-40"
+          placeholder="Test"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.key === 'Enter') {
+              e.preventDefault();
+              set({ test: value });
+            }
+          }}
+        />
+      )}
+      {JSON.stringify(data)}
     </div>
   );
 }
