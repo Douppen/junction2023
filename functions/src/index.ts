abc9032f2020e18f0810e5c8260aca91bec41033
Emil Ekholm <emil.ekholm@aalto.fi>
2023-11-10 23:13:19 +0200

    Added AI chatting for pain input

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -30,3 +30,46 @@ export const OpenAIcompletions = onCall(async (req) => {
   return 'Hello from Firebase!' + text; 
 
 });
+/*
+Implementing AI chatting for getting the variables separated from the description text 
+*/
+/*
+description as input 
+*/
+export const OpenAIAssistantPainInput = onCall(async (req) => {
+  const openai= new OpenAI({ apiKey: "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"});
+  const assistant_id="asst_qTuEb5TistWrBBQrieoRCrpB" 
+  const { description } = req.data;
+  const thread = await openai.beta.threads.create({
+    messages: [
+      { 
+        role: "user",
+        content:  `description: ${description}`,
+      }
+    ]
+  });
+  
+  const run = await openai.beta.threads.runs.create(
+    thread.id,
+    { assistant_id: assistant_id }
+  );
+  
+  let status = "queued";
+  var runStatus= await openai.beta.threads.runs.retrieve(thread.id, run.id);; 
+  
+  
+  while (status !== "completed") {
+    runStatus = await openai.beta.threads.runs.retrieve(thread.id, run.id);
+    status = runStatus.status;
+    logger.log("Status:", status);
+
+      // Add a delay before checking again (e.g., every few seconds)
+      await new Promise(resolve => setTimeout(resolve, 5000))
+   // 5 seconds delay
+  };
+  const messages= await openai.beta.threads.messages.list(thread.id);
+  logger.log(messages.data[0].content);
+ 
+  return messages.data[0].content; 
+
+});
