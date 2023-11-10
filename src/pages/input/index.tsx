6bdbe5bc3cf83fe72b057e29e8641614d5dfa0c3
danielmichaeli <daniel.michaeli@aalto.fi>
2023-11-10 20:40:04 +0200

    .

--- src/pages/input/index.tsx
+++ src/pages/input/index.tsx
@@ -1,9 +1,31 @@
-function Input() {
+// import React, useState, and firebase database
+import React, { useState } from 'react';
+import { firebaseConfig } from '../../lib/firebase';
+import { initializeApp } from 'firebase/app';
+import { getFirestore, addDoc, collection } from 'firebase/firestore';
+
   return (
     <div>
-      <h1>Input</h1>
+      <header>
+        <h1>How have you been feeling recently?</h1>
+        <p>Describe your recent pain level to track your progress.</p>
+        <p>Rate your pain level on a scale from 1-5, describe the area of pain and feel free to add more context.</p>
+      </header>
+
+      {submissionStatus && <p>{submissionStatus}</p>}
+
+      <form onSubmit={handleSubmit}>
+        <label>
+          Pain Description:
+          <input
+            type="text"
+            name="painDescription"
+            value={painDescription}
+            onChange={(e) => setPainDescription(e.target.value)}
+          />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
-}
-
-export default Input;
+};
