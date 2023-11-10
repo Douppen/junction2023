7230c75fc821af0aa654ecc1e13c6807e91f8ef6
danielmichaeli <daniel.michaeli@aalto.fi>
2023-11-10 21:55:25 +0200

    styling

--- src/pages/input/index.tsx
+++ src/pages/input/index.tsx
@@ -22,24 +22,23 @@ function Input() {
       <header>
         <h1 style={{fontSize: '40px', fontWeight: 'bold', marginBottom: '20px'}}>How have you been feeling recently?</h1>
         <p style={{fontSize: '24px', marginBottom: '2px'}}>Describe your recent pain level to track your progress.</p>
-        <p style={{fontSize: '24px', marginBottom: '30px'}}>Rate your pain level on a scale from 1-5, describe the area of pain and feel free to add more context in the text box.</p>
+        <p style={{fontSize: '24px', marginBottom: '80px'}}>Rate your pain level on a scale from 1-5, describe the area of pain and feel free to add more context in the text box.</p>
       </header>
 
-
-      <input type="range" min={1} max="5" value={painLevel} onChange={updateSliderValue} className="range range-error" step="1"/>
-      <div className="w-full flex justify-between text-xs px-2">
-        <span>|</span>
-        <span>|</span>
-        <span>|</span>
-        <span>|</span>
-        <span>|</span>
+      <div style={{ width: '68%' }}>
+        <input type="range" min={1} max="5" value={painLevel} onChange={updateSliderValue} className="range range-error" step="1"/>
+        <div className="w-full flex justify-between text-s px-2">
+          {[1, 2, 3, 4, 5].map((label) => (
+            <span key={label}>{label}</span>
+          ))}
+        </div>
       </div>
 
       {loading ? (
         <span className="loading loading-spinner loading-lg" />
       ) : (
         <textarea
-          className="textarea textarea-bordered w-full max-w-[500px] h-[200px] my-10"
+          className="textarea textarea-bordered w-full max-w-[900px] h-[200px] my-10"
           placeholder="Test"
           value={value}
           onChange={(e) => setValue(e.target.value)}
