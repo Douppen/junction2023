d8148942263c258a5190b562eaabadf7307cc9f0
Joonatan Korpela <joonatan.korpela@aalto.fi>
2023-11-10 23:54:51 +0200

    Improve dashboard

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,19 +1,29 @@
-import { httpsCallable } from 'firebase/functions';
 import Link from 'next/link';
-import { useFunctions } from 'reactfire';
+import { useAuth } from 'reactfire';
 import PainChart from '@/components/Painchart';
 
 function Dashboard() {
-  const functions = useFunctions();
-  const helloWorld = httpsCallable(functions, 'helloWorld');
+  const auth = useAuth();
+
+  const displayName = auth.currentUser?.displayName;
 
   return (
     <div className="flex flex-col items-center">
-      <Link href="input" className="my-40">
-        <button className="btn btn-lg btn-primary">Add entry</button>
-      </Link>
+      <div className="my-40 flex flex-col">
+        <h1 style={{ fontSize: '40px', fontWeight: 'bold', marginBottom: '20px' }}>
+          Welcome back{displayName ? `, ${displayName}!` : '!'}
+        </h1>
+        <p style={{ fontSize: '24px', marginBottom: '2px' }}>How have you been feeling recently?</p>
+        <Link href="input" className="self-center mt-10">
+          <button className="btn btn-lg btn-primary">Add entry</button>
+        </Link>
+      </div>
+      <h1 style={{ fontSize: '40px', fontWeight: 'bold', marginBottom: '20px' }}>History</h1>
       <PainChart />
-      <button onClick={() => helloWorld({ text: 'hellooo' })}>Hello world</button>
+      <div className="my-40">
+        <h1 style={{ fontSize: '40px', fontWeight: 'bold', marginBottom: '20px' }}>Personal insights</h1>
+        <p style={{ fontSize: '24px', marginBottom: '2px' }}>Try to do some yoga</p>
+      </div>
     </div>
   );
 }
--- src/pages/input/index.tsx
+++ src/pages/input/index.tsx
@@ -1,7 +1,5 @@
 import { useUserDocument } from '@/lib/store';
-import { StringFormat } from 'firebase/storage';
 import { useEffect, useState } from 'react';
-
 
 function Input() {
   const { data, status, set } = useUserDocument('SpgDdCaYeUSU6Nt9MzlW');
@@ -19,15 +17,27 @@ function Input() {
 
   return (
     <div className="flex flex-col items-center">
-
       <header>
-        <h1 style={{fontSize: '40px', fontWeight: 'bold', marginBottom: '20px'}}>How have you been feeling recently?</h1>
-        <p style={{fontSize: '24px', marginBottom: '2px'}}>Describe your recent pain level to track your progress.</p>
-        <p style={{fontSize: '24px', marginBottom: '80px'}}>Rate your pain level on a scale from 1-5, describe the area of pain and feel free to add more context in the text box.</p>
+        <h1 style={{ fontSize: '40px', fontWeight: 'bold', marginBottom: '20px' }}>
+          How have you been feeling recently?
+        </h1>
+        <p style={{ fontSize: '24px', marginBottom: '2px' }}>Describe your recent pain level to track your progress.</p>
+        <p style={{ fontSize: '24px', marginBottom: '80px' }}>
+          Rate your pain level on a scale from 1-5, describe the area of pain and feel free to add more context in the
+          text box.
+        </p>
       </header>
 
       <div style={{ width: '68%' }}>
-        <input type="range" min={1} max="5" value={painLevel} onChange={updateSliderValue} className="range range-error" step="1"/>
+        <input
+          type="range"
+          min={1}
+          max="5"
+          value={painLevel}
+          onChange={updateSliderValue}
+          className="range range-error"
+          step="1"
+        />
         <div className="w-full flex justify-between text-s px-2">
           {[1, 2, 3, 4, 5].map((label) => (
             <span key={label}>{label}</span>
