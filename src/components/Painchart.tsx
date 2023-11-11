22b14e9940b86f77a164e945568ff5283cf3f375
Luukas Pörtfors <lajp@iki.fi>
2023-11-11 20:43:27 +0200

    feat: use real data for dashboard chart and add input data to assistant

--- src/components/Painchart.tsx
+++ src/components/Painchart.tsx
@@ -2,86 +2,48 @@ import _ from 'lodash';
 
 import { BarChart } from '@mui/x-charts/BarChart';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
-import { useTheme } from '@emotion/react';
 import { useWindowSize } from '@/lib/utils';
+import { useAuth } from 'reactfire';
+import { useEffect, useState } from 'react';
+import { useFirestore } from 'reactfire';
 
-type painLevelData = {
+import { collection, doc, getDocs } from 'firebase/firestore';
+import { useRouter } from 'next/router';
+
+
+export type painLevelData = {
   painLevel: number;
   date: Date;
 };
 
 const graphData: painLevelData[] = [
   {
-    painLevel: 2,
+    painLevel: 0,
     date: new Date('2023-11-03T10:20:30Z'),
   },
   {
-    painLevel: 3,
+    painLevel: 0,
     date: new Date('2023-11-04T09:15:00Z'),
   },
   {
-    painLevel: 1,
+    painLevel: 0,
     date: new Date('2023-11-05T12:45:00Z'),
   },
   {
-    painLevel: 4,
+    painLevel: 0,
     date: new Date('2023-11-06T08:30:00Z'),
   },
   {
-    painLevel: 2,
+    painLevel: 0,
     date: new Date('2023-11-07T11:00:00Z'),
   },
   {
-    painLevel: 5,
+    painLevel: 0,
     date: new Date('2023-11-08T15:20:00Z'),
   },
   {
-    painLevel: 1,
+    painLevel: 0,
     date: new Date('2023-11-09T07:50:00Z'),
-  },
-  {
-    painLevel: 3,
-    date: new Date('2023-11-10T14:10:00Z'),
-  },
-  {
-    painLevel: 4,
-    date: new Date('2023-11-11T10:00:00Z'),
-  },
-  {
-    painLevel: 2,
-    date: new Date('2023-11-12T13:30:00Z'),
-  },
-  {
-    painLevel: 3,
-    date: new Date('2023-11-13T09:45:00Z'),
-  },
-  {
-    painLevel: 1,
-    date: new Date('2023-11-14T10:55:00Z'),
-  },
-  {
-    painLevel: 4,
-    date: new Date('2023-11-15T08:20:00Z'),
-  },
-  {
-    painLevel: 1,
-    date: new Date('2023-11-15T08:20:00Z'),
-  },
-  {
-    painLevel: 2,
-    date: new Date('2023-11-16T11:10:00Z'),
-  },
-  {
-    painLevel: 5,
-    date: new Date('2023-11-17T14:30:00Z'),
-  },
-  {
-    painLevel: 3,
-    date: new Date('2023-11-19T13:00:00Z'),
-  },
-  {
-    painLevel: 4,
-    date: new Date('2023-11-20T10:25:00Z'),
   },
 ];
 
@@ -119,13 +81,45 @@ const preProcess = (data: painLevelData[]) => {
 };
 
 function PainChart() {
-  // TODO: fetch real data here
-  const preProcessed = preProcess(graphData).slice(-7);
+  const router = useRouter();
+  const auth = useAuth();
+  if (!auth.currentUser) {
+    router.push("/login")
+    return
+  }
+
+  const db = useFirestore();
+
+  const [data, setData] = useState(new Array())
+
+  useEffect(() => {
+    const getData = async () => {
+      const painLevelRef = collection(doc(collection(db, 'users'), auth.currentUser?.uid), 'painLevels');
+      const painLevels = await getDocs(painLevelRef)
+      const painLevelData: painLevelData[] = []
+      painLevels.forEach(p => {
+        const data = p.data()
+        const painLevel = data.painLevel
+        const date = data.date.toDate()
+        if (painLevel && date) {
+          painLevelData.push({painLevel: painLevel, date: date})
+          }
+      })
+      const preProcessed = preProcess(painLevelData).slice(-7);
+      setData(preProcessed)
+    }
+
+    getData()
+  })
 
   const mode = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
   const theme = createTheme({ palette: { mode } });
 
   const [width] = useWindowSize();
+
+  if (data.length == 0) {
+    return
+  }
 
   return (
     <ThemeProvider theme={theme}>
@@ -133,13 +127,13 @@ function PainChart() {
         xAxis={[
           {
             id: 'barCategories',
-            data: preProcessed.map((d) => d.date.toDateString()),
+            data: data.map((d) => d.date.toDateString()),
             scaleType: 'band',
           },
         ]}
         series={[
           {
-            data: preProcessed.map((d) => d.pain),
+            data: data.map((d) => d.pain),
             label: 'Average pain level',
           },
         ]}
