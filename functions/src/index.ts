d4497a36b9df30886bf9e26f1d82d5571e9c2f1b
Emil Ekholm <emil.ekholm@aalto.fi>
2023-11-11 11:28:55 +0200

    Create user thread function that is initially run and adds the user data is added on the backend

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -21,14 +21,14 @@ export const OpenAIcompletions = onCall(async (req) => {
     prompt: 'Say this is a test.',
     max_tokens: 7,
     temperature: 0,
-  });
-  logger.log(completion); // It prints the completed message to the backend
+    });
+  logger.log(completion); // It prints the completed message to the backend 
   /*
   The Request response stuff
   */
   const { text } = req.data;
-
-  return 'Hello from Firebase!' + text;
+ 
+  return 'Hello from Firebase!' + text; 
 });
 /*
 Implementing AI chatting for getting the variables separated from the description text 
@@ -38,36 +38,55 @@ description as input
 */
 export const OpenAIAssistantPainInput = onCall(async (req) => {
   const openai = new OpenAI({ apiKey: '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' });
-  const assistant_id = 'asst_qTuEb5TistWrBBQrieoRCrpB';
+  const assistant_id = 'asst_qTuEb5TistWrBBQrieoRCrpB'; 
   const { description } = req.data;
   const thread = await openai.beta.threads.create({
     messages: [
-      {
+      { 
         role: 'user',
         content: `description: ${description}`,
       },
     ],
   });
-
+  
   const run = await openai.beta.threads.runs.create(thread.id, { assistant_id: assistant_id });
-
+  
   let status = 'queued';
-  var runStatus = await openai.beta.threads.runs.retrieve(thread.id, run.id);
-
-  logger.log('before loop');
-
+  var runStatus = await openai.beta.threads.runs.retrieve(thread.id, run.id); 
+  
+logger.log('before loop');
+  
   while (status !== 'completed') {
     runStatus = await openai.beta.threads.runs.retrieve(thread.id, run.id);
     status = runStatus.status;
     logger.log('Status:', status);
 
-    // Add a delay before checking again (e.g., every few seconds)
-    await new Promise((resolve) => setTimeout(resolve, 500));
-    // 5 seconds delay
+      // Add a delay before checking again (e.g., every few seconds)
+      await new Promise((resolve) => setTimeout(resolve, 500));
+   // 5 seconds delay
   }
   logger.log('after loop');
   const messages = await openai.beta.threads.messages.list(thread.id);
   logger.log(messages.data[0].content);
-
-  return messages.data[0].content;
+ 
+  return messages.data[0].content; 
 });
+export const CreateUserThread = onCall(async (req) => {
+  const {userdata} = req.data; 
+  const openai= new OpenAI({apiKey: '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' });
+  const thread = await openai.beta.threads.create({
+    messages: [
+      { 
+        role: 'user',
+        content: `Data of the specific user: ${userdata}`,
+      },
+    ],
+  });
+  return thread.id
+});
+
+export const OpenAIAddmessagetoBigAssistant= onCall( async (req) => {
+  const openai = new OpenAI({ apiKey: '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' });
+  const assistant_id = 'asst_dOTLaf0iSAjwbvK5ULNT2nfE'; 
+  const threadID= 
+})
