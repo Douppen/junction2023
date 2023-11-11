ca0b98267dd74eddedeb2bc0e03d2eda02e1a91f
Simon Nars <simon.nars@gmail.com>
2023-11-11 10:28:04 +0200

    auth emulator connect try

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,7 @@
 import { setGlobalOptions } from 'firebase-functions/v2/options';
 import { onCall } from 'firebase-functions/v2/https';
 import * as logger from 'firebase-functions/logger';
-import OpenAI from "openai";
+import OpenAI from 'openai';
 
 const DEFAULT_REGION = 'europe-west1';
 setGlobalOptions({ region: DEFAULT_REGION });
@@ -14,21 +14,20 @@ export const helloWorld = onCall((req) => {
 });
 
 export const OpenAIcompletions = onCall(async (req) => {
-  const openai= new OpenAI({ apiKey: "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"});
-  const completion= await openai.completions.create({
-    model: "gpt-3.5-turbo-instruct",
-    prompt: "Say this is a test.",
+  const openai = new OpenAI({ apiKey: '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' });
+  const completion = await openai.completions.create({
+    model: 'gpt-3.5-turbo-instruct',
+    prompt: 'Say this is a test.',
     max_tokens: 7,
     temperature: 0,
-    });
-  logger.log(completion); // It prints the completed message to the backend 
+  });
+  logger.log(completion); // It prints the completed message to the backend
   /*
   The Request response stuff
   */
   const { text } = req.data;
- 
-  return 'Hello from Firebase!' + text; 
 
+  return 'Hello from Firebase!' + text;
 });
 /*
 Implementing AI chatting for getting the variables separated from the description text 
@@ -37,39 +36,37 @@ Implementing AI chatting for getting the variables separated from the descriptio
 description as input 
 */
 export const OpenAIAssistantPainInput = onCall(async (req) => {
-  const openai= new OpenAI({ apiKey: "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"});
-  const assistant_id="asst_qTuEb5TistWrBBQrieoRCrpB" 
+  const openai = new OpenAI({ apiKey: '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' });
+  const assistant_id = 'asst_qTuEb5TistWrBBQrieoRCrpB';
   const { description } = req.data;
   const thread = await openai.beta.threads.create({
     messages: [
-      { 
-        role: "user",
-        content:  `description: ${description}`,
-      }
-    ]
+      {
+        role: 'user',
+        content: `description: ${description}`,
+      },
+    ],
   });
-  
-  const run = await openai.beta.threads.runs.create(
-    thread.id,
-    { assistant_id: assistant_id }
-  );
-  
-  let status = "queued";
-  var runStatus= await openai.beta.threads.runs.retrieve(thread.id, run.id);; 
-  
-  
-  while (status !== "completed") {
+
+  const run = await openai.beta.threads.runs.create(thread.id, { assistant_id: assistant_id });
+
+  let status = 'queued';
+  var runStatus = await openai.beta.threads.runs.retrieve(thread.id, run.id);
+
+  logger.log('before loop');
+
+  while (status !== 'completed') {
     runStatus = await openai.beta.threads.runs.retrieve(thread.id, run.id);
     status = runStatus.status;
-    logger.log("Status:", status);
+    logger.log('Status:', status);
 
-      // Add a delay before checking again (e.g., every few seconds)
-      await new Promise(resolve => setTimeout(resolve, 5000))
-   // 5 seconds delay
-  };
-  const messages= await openai.beta.threads.messages.list(thread.id);
+    // Add a delay before checking again (e.g., every few seconds)
+    await new Promise((resolve) => setTimeout(resolve, 500));
+    // 5 seconds delay
+  }
+  logger.log('after loop');
+  const messages = await openai.beta.threads.messages.list(thread.id);
   logger.log(messages.data[0].content);
- 
-  return messages.data[0].content; 
 
+  return messages.data[0].content;
 });
